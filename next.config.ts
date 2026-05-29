40f207d9da3387db6dbab66d195177917295d0dc
nexioraai <issayamiyoussouf@gmail.com>
2026-05-29 09:15:11 -0400

    fix: add pexels domains to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.pexels.com', 'videos.pexels.com'],
+  },
 };
-
 export default nextConfig;
