873dce948b9d75d29493293861ad75d38ec45001
nexioraai <issayamiyoussouf@gmail.com>
2026-06-16 16:07:08 -0400

    Masquer liens connexion/inscription navbar sur accueil (redondance)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -112,7 +112,7 @@ export default function Navbar() {
               <MenuIcon size={20} />
             </button>
           ) : (
-            <>
+            !isHome && <>
               <Link href="/login" className="text-white/70 hover:text-white text-sm font-medium transition-colors hidden sm:inline">{t('nav.login')}</Link>
               <Link href="/signup" className="btn-nexiora px-4 sm:px-5 py-2 rounded-full text-white text-sm font-semibold whitespace-nowrap">{t('nav.signup')}</Link>
             </>
